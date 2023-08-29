9623d39cdc5547fc2e36a1a1459e9d0df7f987ac
ruheee <Ruidantas55@gmail.com>
2023-08-29 11:18:10 -0400

    fixing github push and pull

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -170,7 +170,7 @@ export default function Home() {
             </p>
             <a
               href="https://github.com/Ruheee/scheduler"
-              target="_blank"
+              target="_blank" 
               className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md"
             >
               Git
