0ef2f33962c7844064f69a9ef52ddaa87442f1de
Rui Dantas <ruidantas55@gmail.com>
2023-11-08 10:40:30 -0500

    Update Rresume

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  href="public/Resume.pdf" download="Ruis Resume"
+                  href="RuisResume.pdf" target={"_blank"}
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                 >
                   Resume
